edc617783054f59d32f45350b72fbccb7edcafd4
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-01-21 02:15:34 -0300

    proximo passo gerar tela de usuário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
+import { UserComponent } from './user/user.component';
+
+
 export const appRoutes: Routes = [
-  { path: '', component: BemVindoComponent }
+  { path: '', component: BemVindoComponent },
+  { path: 'user', component: UserComponent },
 ];
