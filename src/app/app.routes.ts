f06acffd8b7159ec0f37ac508b3ac8f3ad65bd06
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-01-21 02:09:21 -0300

    Salvar alterações antes de modificar o componente de usuário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: BemVindoComponent }
+];
